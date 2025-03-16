eeefdbc21134a99aebee350bad82b0accc4841e5
Gabriel Silva <gabrielssantana41@gmail.com>
2025-03-16 20:48:23 -0300

    att01

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -8,7 +8,6 @@ const projects: ProjectCardProps[] = [
     description: "Um site completo de delivery construído com React, Node.js e JavaScript, incluindo sistema de pagamento, carrinho e muito mais.",
     image: "https://images.unsplash.com/photo-1600728619239-d2a73f7aa541?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     tags: ["React", "Node.js", "JavaScript"],
-    liveUrl: "https://example.com/ecommerce",
     repoUrl: "https://github.com/Gabriel-dev10/Maricafood-Html--Css--Js--React",
   },
   {
@@ -17,7 +16,6 @@ const projects: ProjectCardProps[] = [
     description: "Aplicativo de sorteio construido em Kotlin com Android Studio.",
     image: "https://plus.unsplash.com/premium_photo-1718090610890-dffeae62e46a?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     tags: ["Kotlin", "Android Studio"],
-    liveUrl: "https://example.com/task-manager",
     repoUrl: "https://github.com/Gabriel-dev10/AppSorteio-AndroidStudio--Kotlin",
   },
   {
@@ -26,7 +24,6 @@ const projects: ProjectCardProps[] = [
     description: "Um sistema de árvore binária para filtragem de dados.",
     image: "https://images.unsplash.com/photo-1728995025396-b5141e209455?q=80&w=1859&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     tags: ["Python"],
-    liveUrl: "https://example.com/photography",
     repoUrl: "https://github.com/Gabriel-dev10/Grafos-Python",
   },
   {
@@ -35,7 +32,6 @@ const projects: ProjectCardProps[] = [
     description: "Famosa pokédex em Html, Css e JavaScript.",
     image: "https://images.unsplash.com/photo-1621568670868-24a7dfc590e9?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     tags: ["Html", "Css", "JavaScript"],
-    liveUrl: "https://example.com/finance-dashboard",
     repoUrl: "https://github.com/Gabriel-dev10/projetoPokemon",
   },
   {
@@ -44,7 +40,6 @@ const projects: ProjectCardProps[] = [
     description: "Um jogo.",
     image: "https://images.unsplash.com/photo-1541278107931-e006523892df?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     tags: ["Html", "Css", "JavaScript"],
-    liveUrl: "https://example.com/finance-dashboard",
     repoUrl: "https://github.com/Gabriel-dev10/jogoF5",
   },
 ];
